b442f0e73615a12a8d251c0b7fa11cf2ddfbe4c9
Carl Huseby Fosli <carl@fosli.net>
2023-11-09 11:36:31 +0100

    Create newScratchOrg.bat

---
+++ scripts/bat/newScratchOrg.bat
@@ -0,0 +1,16 @@
+echo "Oppretter scratch org"
+call sf org create scratch --definition-file config\project-scratch-def.json --alias %1 --duration-days %2 --set-default --json --wait 30
+
+echo "Installerer crm-platform-base ver. 0.198"
+call sf package install --package 04t7U000000LPIYQA4 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-access-control ver. 0.113"
+call sf package install --package 04t7U0000004e8tQAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-community-base ver. 0.93"
+call sf package install --package 04t7U000000Y26AQAS --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Dytter kildekoden til scratch org'en"
+call sf project deploy start
+
+echo "Ferdig"
