11ea26279db669834a13131d71933d5b14b18b47
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-11-13 16:19:17 +0100

    Hjem

---
+++ force-app/main/default/lwc/home/home.js
@@ -0,0 +1,36 @@
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import CLAIMSEND from '@salesforce/resourceUrl/TasklistSendIcon';
+import FIGURE from '@salesforce/resourceUrl/Figure';
+import TASKLIST from '@salesforce/resourceUrl/TaskList';
+import FOLDER from '@salesforce/resourceUrl/Folder';
+import PERSON from '@salesforce/resourceUrl/Person';
+import INFORMATION from '@salesforce/resourceUrl/Information';
+
+export default class Hot_home extends NavigationMixin(LightningElement) {
+    nyttKravImg = CLAIMSEND;
+    mineVedtakImg = FIGURE;
+    mineKravTilGodkjenningImg = TASKLIST;
+    mineKravImg = FOLDER;
+    mineSiderImg = PERSON;
+    veiledningImg = INFORMATION;
+
+    @track pageLinks = {};
+    @track waitingClaims = true;
+
+    connectedCallback() {
+        sessionStorage.clear(); // Clear session storage when on home
+        window.scrollTo(0, 0);
+        let baseURLArray = window.location.pathname.split('/');
+        baseURLArray.pop();
+        let baseURL = baseURLArray.join('/');
+        this.pageLinks = {
+            newClaim: baseURL + '/nytt-krav',
+            myRequests: baseURL + '/innsendte-krav',
+            myRequestsOther: baseURL + '/mine-vedtak',
+            myPage: baseURL + '/mien-krav',
+            myThreads: baseURL + '/mine-sider',
+            veiledning: 'https://www.nav.no/lese-og-sekretaerhjelp'
+        };
+    }
+}
