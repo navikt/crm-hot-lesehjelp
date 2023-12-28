af772cc6d839c6a04e57710f2e0ee95cc8c8962a
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-12-28 10:01:35 +0100

    .

--- force-app/main/default/lwc/hot_entitlementList/hot_entitlementList.js
+++ force-app/main/default/lwc/hot_entitlementList/hot_entitlementList.js
@@ -1,7 +1,6 @@
 import { LightningElement, track, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
-import getMyClaims from '@salesforce/apex/HOT_ClaimController.getMyClaims';
-import getClaimLineItems from '@salesforce/apex/HOT_ClaimLineItemController.getClaimLineItems';
+import getEntitlements from '@salesforce/apex/HOT_EntitlementController.getEntitlements';
 
 export default class Hot_entitlementList extends LightningElement {
     @track showEntitlementlist = true;
@@ -24,77 +23,41 @@ export default class Hot_entitlementList extends LightningElement {
 
     @track record;
     @track isNotCancelable = true;
-    connectedCallback() {
-        refreshApex(this.wiredAllClaim);
-    }
+    connectedCallback() {}
 
-    @track unmappedClaims;
-    @track claims;
+    @track unmappedEntitlements;
+    @track entitlements;
 
-    wiredAllClaim;
-
-    wiredClaimsResult;
-    @wire(getMyClaims)
+    wiredEntitlementsResult;
+    @wire(getEntitlements)
     wiredClaims(result) {
-        this.wiredAllClaim = result;
-        this.wiredClaimsResult = result.data;
+        this.wiredEntitlementsResult = result.data;
+        console.log(this.wiredEntitlementsResult);
         if (result.data) {
-            console.log('fant' + this.wiredClaimsResult.length);
-            if (this.wiredClaimsResult.length != 0) {
+            console.log('fant' + this.wiredEntitlementsResult.length);
+            if (this.wiredEntitlementsResult.length != 0) {
                 this.noEntitlements = false;
             }
-            this.unmappedClaims = [];
-            this.claims = [];
+            this.unmappedEntitlements = [];
+            this.entitlements = [];
             result.data.forEach((element) => {
-                this.unmappedClaims.push(element);
+                this.unmappedEntitlements.push(element);
             });
-            if (this.unmappedClaims.length != 0) {
+            if (this.unmappedEntitlements.length != 0) {
                 this.noEntitlements = false;
             }
 
-            this.claims = this.unmappedClaims.map((x) => ({
+            this.entitlements = this.unmappedEntitlements.map((x) => ({
                 ...x,
-                created: this.formatDateTime(x.CreatedDate)
+                period: this.formatDateTimePeriod(x.FromDate__c, x.ToDate__c)
             }));
-            this.claims.sort((a, b) => {
-                if (b.CreatedDate === a.CreatedDate) {
-                    return 0;
-                } else {
-                    return b.CreatedDate < a.CreatedDate ? -1 : 1;
-                }
-            });
-        }
-    }
-    checkIfCancelButtonIsDisabled(status) {
-        if (status == 'Sent') {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    formatDateTime(date) {
-        let unformatted = new Date(date);
-        let formattedTime =
-            ('0' + unformatted.getDate()).slice(-2) +
-            '.' +
-            ('0' + (unformatted.getMonth() + 1)).slice(-2) +
-            '.' +
-            unformatted.getFullYear() +
-            ', Kl ' +
-            ('0' + unformatted.getHours()).slice(-2) +
-            ':' +
-            ('0' + unformatted.getMinutes()).slice(-2);
-        if (formattedTime.includes('NaN')) {
-            formattedTime = '';
-        }
-        return formattedTime;
-    }
-    yesOrNoCalculator(string) {
-        if (string == true) {
-            return 'Ja';
-        } else {
-            return 'Nei';
+            // this.claims.sort((a, b) => {
+            //     if (b.CreatedDate === a.CreatedDate) {
+            //         return 0;
+            //     } else {
+            //         return b.CreatedDate < a.CreatedDate ? -1 : 1;
+            //     }
+            // });
         }
     }
     formatDateTimePeriod(dateFrom, dateTo) {
@@ -107,14 +70,13 @@ export default class Hot_entitlementList extends LightningElement {
             ('0' + (unformattedFrom.getMonth() + 1)).slice(-2) +
             '.' +
             unformattedFrom.getFullYear() +
-            ', Kl ' +
-            ('0' + unformattedFrom.getHours()).slice(-2) +
-            ':' +
-            ('0' + unformattedFrom.getMinutes()).slice(-2) +
             ' - ' +
-            ('0' + unformattedTo.getHours()).slice(-2) +
-            ':' +
-            ('0' + unformattedTo.getMinutes()).slice(-2);
+            ('0' + unformattedTo.getDate()).slice(-2) +
+            '.' +
+            ('0' + (unformattedTo.getMonth() + 1)).slice(-2) +
+            '.' +
+            unformattedTo.getFullYear();
+
         if (formattedTime.includes('NaN')) {
             formattedTime = '';
         }
