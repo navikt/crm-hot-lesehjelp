92cad50d4e7b4f0c23b560112fdf62762e0aadea
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2024-02-02 11:19:54 +0100

    .

--- force-app/main/default/lwc/hot_warningBannerOnClaim/hot_warningBannerOnClaim.js
+++ force-app/main/default/lwc/hot_warningBannerOnClaim/hot_warningBannerOnClaim.js
@@ -1,11 +1,12 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import getClaim from '@salesforce/apex/HOT_ClaimController.getClaim';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 export default class Hot_warningBannerOnClaim extends LightningElement {
     @api recordId;
     @track record;
     @track noEntitlement;
+    @track exceedsEntitlementHours = false;
+    @track remainingTimeSum;
 
     wiredResult;
     @wire(getClaim, {
@@ -13,12 +14,24 @@ export default class Hot_warningBannerOnClaim extends LightningElement {
     })
     wiredResult(result) {
         if (result.data) {
-            console.log(result.data[0].Entitlement__c);
-            if (result.data[0].Entitlement__c == '' || result.data[0].Entitlement__c == undefined) {
+            this.record = result.data[0];
+            if (this.record.Entitlement__c == '' || this.record.Entitlement__c == undefined) {
                 this.noEntitlement = true;
             } else {
                 this.noEntitlement = false;
+
+                const entitledHours = this.record.Entitlement__r ? this.record.Entitlement__r.EntitledHours__c : null;
+                const entitledHoursUsed = this.record.Entitlement__r
+                    ? this.record.Entitlement__r.EntitledHoursUsed__c
+                    : null;
+                this.remainingTimeSum = entitledHours - entitledHoursUsed;
+                if (this.record.TotalHours__c >= this.remainingTimeSum) {
+                    this.exceedsEntitlementHours = true;
+                } else {
+                    this.exceedsEntitlementHours = false;
+                }
             }
+            console.log(result.data);
         }
     }
 }
